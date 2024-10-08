d066723574a0c2ba8ba85def1fde92a968bbb6bf
Devaraju93 <devrajg9318@gmail.com>
2024-10-08 17:39:44 +0530

    sidebar done

--- app/Pages/AllProjects/AllProjects.tsx
+++ app/Pages/AllProjects/AllProjects.tsx
@@ -1,11 +1,14 @@
+import AllProjectsSection from "./Components/AllProjectsSection";
 import ProjectsHeader from "./Components/ProjectsHeader";
 import ProjectsSubHeader from "./Components/ProjectsSubHeader";
+import StatsRightSideBar from "./Components/StatsRightSideBar";
 
 
 function AllProjects() {
   return (
     <div className="bg-slate-50 w-full min-h-screen flex">
       <AllProjectsArea />
+      <StatsRightSideBar/>
     </div>
   );
 }
@@ -17,6 +20,8 @@ function AllProjectsArea() {
       <ProjectsHeader />
       {/* my projects tittle and the add button */}
       <ProjectsSubHeader />
+      {/* all projects added */}
+      <AllProjectsSection />
     </div>
   );
 }
