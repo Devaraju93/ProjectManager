0f719c7d41e6a3d76eeca1d5af6c5db737147afc
Devaraju93 <devrajg9318@gmail.com>
2024-10-14 18:10:51 +0530

    all tasks containe ui done

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import Sidebar from "./Components/Sidebar";
 import React from "react";
 import AllProjects from "./Pages/AllProjects/AllProjects";
+import AllTasksContainer from "./Pages/AllTasks/AllTasks";
 
 export default function Home() {
   return (
   <div className="flex w-full h-screen poppins bg-white">
+    {/* sidebar */}
     <Sidebar/>
+    {/* All Projects Page */}
     <AllProjects/> 
+    <AllTasksContainer/>
     </div>
   )
 }
